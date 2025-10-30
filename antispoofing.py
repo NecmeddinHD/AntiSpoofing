f15fce80d06b4dca583a06bbfc0341d8c261baa4
NecmeddinHD <necmeddingencanhd@gmail.com>
2025-10-31 00:04:34 +0300

    Add files via upload

---
+++ antispoofing.py
@@ -0,0 +1,33 @@
+import socket, time, os, re
+from plyer import notification
+from collections import Counter
+
+mac_re = re.compile(r"([0-9a-fA-F]{2}[-:][0-9a-fA-F]{2}[-:][0-9a-fA-F]{2}[-:][0-9a-fA-F]{2}[-:][0-9a-fA-F]{2}[-:][0-9a-fA-F]{2})")
+
+facebook = socket.gethostbyname("facebook.com")
+instagram = socket.gethostbyname("instagram.com")
+linkedin = socket.gethostbyname("linkedin.com")
+while True:
+    try:
+        if socket.gethostbyname("facebook.com") == facebook and socket.gethostbyname("instagram.com") == instagram and socket.gethostbyname("linkedin.com") == linkedin:
+            continue
+        else:
+            notification.notify(
+            title="Dikkatli Ol",
+            message="Dikkat şu anda eğer bir devlet ağında değilseniz, Lütfen facebook, instagram, vb. bir sitede oturum açmadan önce sitenin doğruluğunu kontrol edin",
+            timeout=5)
+        arp_output = os.popen("arp -a").read()
+        macs = [m.replace("-", ":").lower() for m in mac_re.findall(arp_output)]
+        duplicates = {m: c for m, c in Counter(macs).items() if c > 2}
+
+        if duplicates:
+            if "ff:ff:ff:ff:ff:ff" not in duplicates:
+                notification.notify(
+                    title="Dikkatli Ol",
+                    message="Ağınızda aynı MAC adresine sahip birden fazla cihaz var! Lütfen HTTP kullanan sitelere bilgilerinizi vermeyiniz.",
+                    timeout=5)
+    except KeyboardInterrupt:
+        break
+    except Exception:
+        continue
+    time.sleep(10)
