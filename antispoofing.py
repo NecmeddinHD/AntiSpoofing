b9402b5619945a273f1a179a3a967694e7a31e09
Hosx8664 <necmeddingencanhd@gmail.com>
2025-11-12 11:32:41 +0300

    Refactor antispoofing.py for clarity and consistency

--- antispoofing.py
+++ antispoofing.py
@@ -2,10 +2,12 @@ import socket, time, os, re
 from plyer import notification
 from collections import Counter
 from typing import Final
+
 mac_re = re.compile(r"([0-9a-fA-F]{2}[-:][0-9a-fA-F]{2}[-:][0-9a-fA-F]{2}[-:][0-9a-fA-F]{2}[-:][0-9a-fA-F]{2}[-:][0-9a-fA-F]{2})")
 facebook: Final = socket.gethostbyname("facebook.com")
 instagram: Final = socket.gethostbyname("instagram.com")
 linkedin: Final = socket.gethostbyname("linkedin.com")
+
 while True:
     try:
         if socket.gethostbyname("facebook.com") == facebook and socket.gethostbyname("instagram.com") == instagram and socket.gethostbyname("linkedin.com") == linkedin:
@@ -15,6 +17,7 @@ while True:
             title="Dikkatli Ol",
             message="Dikkat şu anda eğer bir devlet ağında değilseniz, Lütfen facebook, instagram, vb. bir sitede oturum açmadan önce sitenin doğruluğunu kontrol edin",
             timeout=5)
+            
         arp_output = os.popen("arp -a").read()
         macs = [m.replace("-", ":").lower() for m in mac_re.findall(arp_output)]
         duplicates = {m: c for m, c in Counter(macs).items() if c > 2}
@@ -28,5 +31,7 @@ while True:
         break
     except Exception:
         continue
+        
     time.sleep(10)
+
 
